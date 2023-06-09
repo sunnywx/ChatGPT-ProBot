5ab27a3100aa6619353b78b7dd301d28c4d0b6c7
sunnywx <iwisunny@gmail.com>
2023-06-09 20:23:16 +0800

    refine: remove private key override
    Signed-off-by: sunnywx <iwisunny@gmail.com>

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,10 +1,6 @@
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../dist/index.js");
 module.exports = createNodeMiddleware(app, {
-  probot: createProbot({
-    overrides: {
-      privateKey: Buffer.from(process.env.PRIVATE_KEY, "base64").toString(),
-    }
-  }),
+  probot: createProbot(),
   webhooksPath: "/api/github/webhooks",
 });
